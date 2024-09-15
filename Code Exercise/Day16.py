df856bd6be67ff604569a354ac5a6d50278c36e7
sahil76 <jindalsahil798@gmail.com>
2024-09-15 15:37:16 +0530

    Implemented the Complete and Exit buttons

--- Code Exercise/Day16.py
+++ Code Exercise/Day16.py
@@ -1,16 +1,29 @@
 import FreeSimpleGUI as sg
+from meter_converter import converter_meter
 
 label1 = sg.Text("Enter feet:")
-input1 = sg.Input()
+input1 = sg.Input(key="feet")
 
 label2 = sg.Text("Enter inches:")
-input2 = sg.Input()
+input2 = sg.Input(key="inches")
 
 conver_button = sg.Button("Convert")
+output_label = sg.Text(key="output")
 
 window = sg.Window("Converter", layout=[[label1, input1],
                                         [label2, input2],
-                                        [conver_button]])
+                                        [conver_button, output_label]])
 
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    feet = int(values["feet"])
+    inches = int(values["inches"])
+    meters = converter_meter(feet, inches)
+    print(meters)
+    window["output"].update(value=f"{meters} m", text_color="white")
+
+
+
 window.close()
---
+++ Code Exercise/meter_converter.py
@@ -0,0 +1,5 @@
+def converter_meter(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    # return f"{feet} feet and {inches} inches is equal to {meters} meters"
+    return meters
+
