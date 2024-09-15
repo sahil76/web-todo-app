a3c60cd50e848bb05b289b2b1ef2b098a8e0e0d5
sahil76 <jindalsahil798@gmail.com>
2024-09-16 02:03:30 +0530

    Initial Commit

--- Code Exercise/Day16.py
+++ Code Exercise/Day16.py
@@ -1,6 +1,8 @@
 import FreeSimpleGUI as sg
 from meter_converter import converter_meter
 
+sg.theme("Black")
+
 label1 = sg.Text("Enter feet:")
 input1 = sg.Input(key="feet")
 
@@ -9,20 +11,28 @@ input2 = sg.Input(key="inches")
 
 conver_button = sg.Button("Convert")
 output_label = sg.Text(key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Converter", layout=[[label1, input1],
                                         [label2, input2],
-                                        [conver_button, output_label]])
+                                        [conver_button, output_label],
+                                        [exit_button]])
 
 
 while True:
     event, values = window.read()
-    print(event, values)
-    feet = int(values["feet"])
-    inches = int(values["inches"])
-    meters = converter_meter(feet, inches)
-    print(meters)
-    window["output"].update(value=f"{meters} m", text_color="white")
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+    try:
+        feet = int(values["feet"])
+        inches = int(values["inches"])
+        meters = converter_meter(feet, inches)
+        window["output"].update(value=f"{meters} m", text_color="white")
+    except ValueError:
+        sg.popup("Enter a number first for feet and inches.", font=('Helvetica', 10))
 
 
 
